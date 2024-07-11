43e712ca96d2f0588af5539f6a216e2f93497d5c
paulalima93 <91716314+paulalima93@users.noreply.github.com>
2024-07-11 19:33:39 -0300

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,30 @@
+let questions = [
+    {
+      numb: 1,
+      question: "Quais habilidades a programação desenvolve?",
+      answer: "Resolução de problemas, pensamento lógico e criatividade",
+      options: [
+        "Habilidades de design gráfico",
+        "Habilidades de gerenciamento de projetos",
+        "Resolução de problemas, pensamento lógico e criatividade",
+        "Habilidades de comunicação"
+      ]
+    },
+    
+
+
+
+    // Você pode descomentar o código abaixo e duplicá-lo quantas vezes desejar para adicionar mais perguntas
+    // Lembre-se de dar valores sequenciais para o 'numb' como 1, 2, 3, 4, ...
+    // {
+    //   numb: 1,
+    //   question: "Sua pergunta aqui",
+    //   answer: "Resposta correta da pergunta aqui",
+    //   options: [
+    //     "Opção 1",
+    //     "Opção 2",
+    //     "Opção 3",
+    //     "Opção 4"
+    //   ]
+    // },
+  ];
